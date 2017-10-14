56294fb086b4a83bac1fd7702ae26f77e88de679
Looooopy <giorgio.salmi@stena.com>
2017-10-14 12:22:35 +0200

    * Add new dir on startup of buildAndPatch. * Add comments in the script buildAndPatch.

--- buildAndPatch.sh
+++ buildAndPatch.sh
@@ -1,13 +1,20 @@
+# Temp dir for the nginx build
 mkdir /tmp/spksrc
-
+# Create dir for user settings: reverse proxy,
+# theese copied to app.d when creating a reverse proxy.
+mkdir /etc/nginx/reverseproxy/
+# link our libpcreso ssl works.
 ln -s /usr/lib/libpcre.so /usr/lib/libpcre.so.3
-#chown http:http /etc/nginx/scgi_temp
+# take owenrship of dir as http.
+chown http:http /etc/nginx/scgi_temp
 
+# Build nginx
 docker run -it --rm \
   --name build_nginx1 \
   -v /tmp/spksrc:/spksrc \
   build_nginx:v1.0
 
+# Patch our nginx
 docker run -it --rm \
   --name patch_synology1 \
   -v /tmp/spksrc:/spksrc \
