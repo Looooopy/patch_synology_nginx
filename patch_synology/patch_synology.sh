63862e0d5040831b6670c16c8a9886e5d10c463d
Looooopy <giorgio.salmi@stena.com>
2019-08-04 04:31:43 +0200

    - Fix: Synology 6.2.2-24922-2, Portal.mustache

--- patch_synology/patch_synology.sh
+++ patch_synology/patch_synology.sh
@@ -17,7 +17,7 @@ if [ ! -f /host/bin/nginx.bak ]; then
     sed -i '/server.ReverseProxy.conf" || true/r ./patch_reverse_proxy' /rc.sysv/nginx-conf-generator.sh
 
     echo "Patch Portal.mustache"
-    sed -i '/proxy_http_version      1.1;/r ./patch_portal_mustache_1' /mustache/Portal.mustache
+    sed -i '/{{/advanced}}/r ./patch_portal_mustache_1' /mustache/Portal.mustache
     sed -i '/{{\/letsencrypt}}/r ./patch_portal_mustache_2' ./mustache/Portal.mustache
 
     echo "Patch nginx"
